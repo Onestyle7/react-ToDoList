bb11b47046707ea7ba47819565165d4c667a3ae0
Onestyle7 <klusbartek8@gmail.com>
2024-11-14 14:27:34 +0100

    Added useEffect to storage data in cookies
    Added error handling to check if parsedData is array

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ToDoInput from "./components/ToDoInput";
 import ToDoList from "./components/ToDoList";
 
@@ -6,10 +6,19 @@ function App() {
   const [todos, setTodos] = useState([]); // Stan przechowujący listę zadań
   const [todoValue, setToDoValue] = useState(""); // Stan przechowujący wartość aktualnie wprowadzanego lub edytowanego zadania
 
+  function persistData(newList) {
+    if (Array.isArray(newList)) {
+      localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+    } else {
+      console.error("Próba zapisania danych, które nie są tablicą:", newList);
+    }
+  }
+
   // Funkcja dodająca nowe zadanie do listy
   function handleAddTodos(newTodo) {
     const newToDoList = [...todos, newTodo];
     setTodos(newToDoList);
+    persistData(newToDoList);
   }
 
   // Funkcja usuwająca zadanie z listy na podstawie indeksu
@@ -18,6 +27,7 @@ function App() {
       return todoIndex !== index;
     });
     setTodos(newToDoList);
+    persistData(newToDoList);
   }
 
   // Funkcja edytująca zadanie
@@ -26,6 +36,24 @@ function App() {
     setToDoValue(valueToBeEdited); // Ustawiamy ją w stanie `todoValue`, aby wyświetlić w polu input
     handleDeleteTodo(index); // Usuwamy oryginalne zadanie z listy
   }
+
+  useEffect(() => {
+    let localTodos = localStorage.getItem("todos");
+    if (localTodos) {
+      try {
+        const parsedData = JSON.parse(localTodos);
+        if (Array.isArray(parsedData.todos)) {
+          setTodos(parsedData.todos);
+        } else {
+          console.error("parsedData.todos nie jest tablicą:", parsedData.todos);
+          setTodos([]);
+        }
+      } catch (error) {
+        console.error("Błąd podczas parsowania danych z localStorage:", error);
+        setTodos([]);
+      }
+    }
+  }, []);
 
   return (
     <>
